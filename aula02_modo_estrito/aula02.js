fcb6dcc173e3113581bc0556c19f35f023506774
Nathália Elen <nathaliaelen79@gmail.com>
2024-11-08 23:55:57 -0300

    Modo Estrito

---
+++ aula02_modo_estrito/aula02.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Em modo estrito, o uso de variáveis não declaradas causa erro, entre outras coisas.
+let nome = "Nathália";
+console.log(nome);
+
+let x = 3.14; 
+console.log(x);
+
