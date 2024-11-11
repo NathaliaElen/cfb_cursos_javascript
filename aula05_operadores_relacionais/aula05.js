953ab2b2efc3360294e1b8cfc9750fd8a4349985
Nathália Elen <nathaliaelen79@gmail.com>
2024-11-11 19:59:53 -0300

    Operadores Relacionais

---
+++ aula05_operadores_relacionais/aula05.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//igualdade
+let a = 5, b='5';
+console.log(a == b); //true
+
+//desigualdade
+let c=5, d='5';
+console.log(c != d); // false
+
+//estritamente igual
+let e=5, f='5', g=5;
+console.log(e === f); // false
+console.log(e === g); // true
+
+//estritamente diferente
+let h=5, i='5', j=5;
+console.log(h !== i); // true
+console.log(h !== j); // false
+
+//maior que
+let k=10, l=5;
+console.log(k > l); //true
+
+//menor que
+let m=10, n=5;
+console.log(m < n); //false
+
+//maior ou igual a
+let o=10, p=10;
+console.log(o >= p); //true
+
+//menor ou igual a
+let r=10, s=5;
+console.log(r <= 5); //false
+
+
+
+
