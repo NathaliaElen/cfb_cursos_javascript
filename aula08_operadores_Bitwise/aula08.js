d545076e542224b259b19462640623fd10aeffe3
Nathália Elen <nathaliaelen79@gmail.com>
2024-11-14 20:12:21 -0300

    Operadores Bitwise

---
+++ aula08_operadores_Bitwise/aula08.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let n1=10, n2=11;
+let resAnd = n1 & n2;
+console.log(resAnd);
+
+let n3=10, n4=11;
+let resOr = n1 | n2;
+console.log(resOr);
+
+let n5=10, n6=11;
+let resXor = n5 ^ n6;
+console.log(resXor);
+
+let n7=10;
+let resLeft = n7 << 1
+console.log(resLeft);
+
+let n8=10;
+let resRight = n8 >> 1
+console.log(resRight);
+
+let n9=10;
+let res = n9 >>> 2
+console.log(res);
+
+let n10=10;
+let resNot = ~n10
+console.log(resNot);
+
